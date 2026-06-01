f715b27143009598059ef7b56e2c4acc2b115d7d
ShopKaraokeNow <villagepoocrew@gmail.com>
2026-06-01 11:44:00 -0400

    Allow iframe embedding for Shopify

--- next.config.js
+++ next.config.js
@@ -1,7 +1,24 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+
+  async headers() {
+    return [
+      {
+        source: '/:path*',
+        headers: [
+          {
+            key: 'X-Frame-Options',
+            value: 'ALLOW-FROM https://*.myshopify.com'
+          },
+          {
+            key: 'Content-Security-Policy',
+            value: "frame-ancestors 'self' https://*.myshopify.com https://*.shopify.com"
+          }
+        ]
+      }
+    ]
+  }
 };
 
 module.exports = nextConfig;
