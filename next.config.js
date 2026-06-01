551ca9c2763b1783911e7dd6fb311dcccfdaf176
ShopKaraokeNow <villagepoocrew@gmail.com>
2026-06-01 12:11:39 -0400

    Add iframe headers for Shopify

--- next.config.js
+++ next.config.js
@@ -13,12 +13,13 @@ const nextConfig = {
           },
           {
             key: 'Content-Security-Policy',
-            value: "frame-ancestors 'self' https://*.myshopify.com https://*.shopify.com"
+            value: "frame-ancestors 'self' https://*.myshopify.com https://*.shopify.com https://shopify-live-leaderboard.vercel.app"
           }
         ]
       }
-    ]
+    ];
   }
 };
 
+module.exports = nextConfig;
 module.exports = nextConfig;
